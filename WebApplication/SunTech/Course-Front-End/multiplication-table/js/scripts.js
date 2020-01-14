82d68aae1a042faf6b0ffb6b1d8c2412a79e38b3
tung-nguyen86 <tungnqc0707l.aiti@gmail.com>
2020-01-15 00:48:23 +0700

    Convert Indentation From Tabs To Spaces.

--- WebApplication/SunTech/Course-Front-End/multiplication-table/js/scripts.js
+++ WebApplication/SunTech/Course-Front-End/multiplication-table/js/scripts.js
@@ -25,131 +25,131 @@ var errorContent = EMPTY_STRING; // used for assigning the content value of the 
 /* ------ Start Functions Declaration ------*/
 
 function createMultiplicationTable(startNumber, endNumber, flag = false) {
-	let content = `<table>`;
+    let content = `<table>`;
 
-	for (let x = 1; x <= 10; x++) {
-		content += `<tr>`;
+    for (let x = 1; x <= 10; x++) {
+        content += `<tr>`;
 
-		for (let y = startNumber; y <= endNumber; y++) {
-			if (y % 2 === 0) {
-				content += `<td><font color='red'>${y} x ${x} = ${x * y}</font></td>`;
-			} else {
-				content += `<td><font color='blue'>${y} x ${x} = ${x * y}</font></td>`;
-			}
-		}
+        for (let y = startNumber; y <= endNumber; y++) {
+            if (y % 2 === 0) {
+                content += `<td><font color='red'>${y} x ${x} = ${x * y}</font></td>`;
+            } else {
+                content += `<td><font color='blue'>${y} x ${x} = ${x * y}</font></td>`;
+            }
+        }
 
-		content += `</tr>`;
-	}
+        content += `</tr>`;
+    }
 
-	content += `</table>`;
+    content += `</table>`;
 
-	return content;
+    return content;
 }
 
 function displayMultiplicationTable() {
-	if (!isTableVisible) {
-		if (validateForm()) {
-			isTableVisible = true;
-			divDisplayTable.className = 'visible-table';
-			btnDisplayTable.value = HIDE_TABLE_STRING;
+    if (!isTableVisible) {
+        if (validateForm()) {
+            isTableVisible = true;
+            divDisplayTable.className = 'visible-table';
+            btnDisplayTable.value = HIDE_TABLE_STRING;
 
-			let startNumber = parseInt(txtStartNumber.value.trim());
-			let endNumber = parseInt(txtEndNumber.value.trim());
-			divDisplayTable.innerHTML = createMultiplicationTable(startNumber, endNumber);
+            let startNumber = parseInt(txtStartNumber.value.trim());
+            let endNumber = parseInt(txtEndNumber.value.trim());
+            divDisplayTable.innerHTML = createMultiplicationTable(startNumber, endNumber);
 
-			errorInfo.innerHTML = EMPTY_STRING;
-			errorInfo.className = 'hidden-error-info';
-		} else {
-			isTableVisible = false;
-			divDisplayTable.className = 'hidden-table';
+            errorInfo.innerHTML = EMPTY_STRING;
+            errorInfo.className = 'hidden-error-info';
+        } else {
+            isTableVisible = false;
+            divDisplayTable.className = 'hidden-table';
 
-			errorInfo.innerHTML = errorContent;
-			errorInfo.className = 'visible-error-info';
-		}
-	} else {
-		isTableVisible = false;
-		divDisplayTable.className = 'hidden-table';
-		btnDisplayTable.value = DISPLAY_TABLE_STRING;
-	}
+            errorInfo.innerHTML = errorContent;
+            errorInfo.className = 'visible-error-info';
+        }
+    } else {
+        isTableVisible = false;
+        divDisplayTable.className = 'hidden-table';
+        btnDisplayTable.value = DISPLAY_TABLE_STRING;
+    }
 }
 
 function validateForm() {
-	let startNumber = txtStartNumber.value.trim();
-	let endNumber = txtEndNumber.value.trim();
+    let startNumber = txtStartNumber.value.trim();
+    let endNumber = txtEndNumber.value.trim();
 
-	if (isEmpty(startNumber)) {
-		errorContent = 'Start Number is required !!!';
-		txtStartNumber.focus();
-		return false;
-	} else if (isEmpty(endNumber)) {
-		errorContent = 'End Number is required !!!';
-		txtEndNumber.focus();
-		return false;
-	} else if (isNotNumeric(startNumber)) {
-		errorContent = 'Start Number must be a numeric value !!!';
-		txtStartNumber.focus();
-		return false;
-	} else if (isNotNumeric(endNumber)) {
-		errorContent = 'End Number must be a numeric value !!!';
-		txtEndNumber.focus();
-		return false;
-	}
+    if (isEmpty(startNumber)) {
+        errorContent = 'Start Number is required !!!';
+        txtStartNumber.focus();
+        return false;
+    } else if (isEmpty(endNumber)) {
+        errorContent = 'End Number is required !!!';
+        txtEndNumber.focus();
+        return false;
+    } else if (isNotNumeric(startNumber)) {
+        errorContent = 'Start Number must be a numeric value !!!';
+        txtStartNumber.focus();
+        return false;
+    } else if (isNotNumeric(endNumber)) {
+        errorContent = 'End Number must be a numeric value !!!';
+        txtEndNumber.focus();
+        return false;
+    }
 
-	startNumber = parseInt(startNumber);
-	endNumber = parseInt(endNumber);
-	if (isInvalidValue(startNumber, endNumber, txtStartNumber, txtEndNumber)) {
-		return false;
-	}
+    startNumber = parseInt(startNumber);
+    endNumber = parseInt(endNumber);
+    if (isInvalidValue(startNumber, endNumber, txtStartNumber, txtEndNumber)) {
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 function isEmpty(inputData) {
-	return ((inputData === EMPTY_STRING) || (inputData.length === 0));
+    return ((inputData === EMPTY_STRING) || (inputData.length === 0));
 }
 
 function isNotNumeric(inputData) {
-	return !(/^(\-{0,1})[0-9]+$/.test(inputData));
+    return !(/^(\-{0,1})[0-9]+$/.test(inputData));
 }
 
 function isInvalidValue(startNumber, endNumber, txtStartNumber, txtEndNumber) {
-	if (startNumber < 1 || startNumber > 10) {
-		errorContent = 'Start Number must be an integer from 1 to 10 !!!';
-		txtStartNumber.focus();
-		return true;
-	} else if (endNumber < 1 || endNumber > 10) {
-		errorContent = 'End Number must be an integer from 1 to 10 !!!';
-		txtEndNumber.focus();
-		return true;
-	} else if (startNumber > endNumber) {
-		errorContent = 'Start Number must be less than or equal to End Number !!!';
-		return true;
-	}
+    if (startNumber < 1 || startNumber > 10) {
+        errorContent = 'Start Number must be an integer from 1 to 10 !!!';
+        txtStartNumber.focus();
+        return true;
+    } else if (endNumber < 1 || endNumber > 10) {
+        errorContent = 'End Number must be an integer from 1 to 10 !!!';
+        txtEndNumber.focus();
+        return true;
+    } else if (startNumber > endNumber) {
+        errorContent = 'Start Number must be less than or equal to End Number !!!';
+        return true;
+    }
 
-	return false;
+    return false;
 }
 
 function processOnKeyPress(event) {
-	if (isTableVisible) {
-		isTableVisible = false;
-		btnDisplayTable.value = DISPLAY_TABLE_STRING;
-	}
+    if (isTableVisible) {
+        isTableVisible = false;
+        btnDisplayTable.value = DISPLAY_TABLE_STRING;
+    }
 
-	// Handle when end-user press Enter key on the keyboard.
-	if (event.keyCode == 13) {
-		event.preventDefault();
-		displayMultiplicationTable();
-	}
+    // Handle when end-user press Enter key on the keyboard.
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        displayMultiplicationTable();
+    }
 }
 
 function processOnKeyUp(event) {
-	// Handle when end-user press Backspace or Delete key on the keyboard.
-	if (event.keyCode == 8 || event.keyCode == 46) {
-		if (!validateForm()) {
-			event.stopPropagation();
-			btnDisplayTable.value = DISPLAY_TABLE_STRING;
-		}
-	}
+    // Handle when end-user press Backspace or Delete key on the keyboard.
+    if (event.keyCode == 8 || event.keyCode == 46) {
+        if (!validateForm()) {
+            event.stopPropagation();
+            btnDisplayTable.value = DISPLAY_TABLE_STRING;
+        }
+    }
 }
 
 /* ------ End Functions Declaration ------*/
